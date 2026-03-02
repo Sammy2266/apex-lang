6fc794cb5fca8f32d6a04fff99bcef7c732b9ccb
Sammy2266 <sammymuchai44@gmail.com>
2026-03-02 19:33:39 +0300

    feat: Implement initial compiler structure including lexer, parser, AST, semantic analyzer, AI lowering, and LLVM backend.

---
+++ compiler/target/debug/deps/apex_compiler.d
@@ -0,0 +1,5 @@
+C:\Users\Peter\apex-lang\compiler\target\debug\deps\apex_compiler.d: src\main.rs
+
+C:\Users\Peter\apex-lang\compiler\target\debug\deps\apex_compiler.exe: src\main.rs
+
+src\main.rs:
